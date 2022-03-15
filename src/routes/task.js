bec4146367312e67e877336156ba2dff49480e2f
Muhammad Bilal <bilal.soft000@gmail.com>
2022-03-15 18:13:19 +0500

    sorting and pagination

--- src/routes/task.js
+++ src/routes/task.js
@@ -16,15 +16,29 @@ route.post("/tasks", auth, async (req, res) => {
   }
 });
 
+// GET /tasks?complted=true
+// GET /tasks?limit=10&skip=0
+// GET /tasks?createdAt:desc
+
 route.get("/tasks", auth, async (req, res) => {
   const match = {};
-
+  const sort = {};
   if (req.query.completed) match.completed = req.query.completed === "true";
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+  }
 
   try {
     await req.user.populate({
       path: "tasks",
       match,
+      options: {
+        limit: req?.query?.limit && parseInt(req.query.limit),
+        skip: req?.query?.skip && parseInt(req.query.skip),
+        sort,
+      },
     });
     res.send(req.user.tasks);
   } catch (error) {
